d63943f296442ff1ed5d18018db6aefdc257c7bd
Braendeholm <chrissword8@gmail.com>
2025-12-01 10:29:45 +0100

    Initial commit

---
+++ src/main/java/dk/ek/onidesign/OniDesignApplication.java
@@ -0,0 +1,15 @@
+package dk.ek.onidesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OniDesignApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OniDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ek/onidesign/OniDesignApplicationTests.java
@@ -0,0 +1,15 @@
+package dk.ek.onidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OniDesignApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
