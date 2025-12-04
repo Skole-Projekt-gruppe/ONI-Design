2da42176f108f8e915ca99c22195c03b5274dd14
asoeb <a.soeborg-skau@hotmail.dk>
2025-12-04 12:47:05 +0100

    Repositories

--- src/main/java/dk/ek/onidesign/catalog/repository/PackDataRepository.java
+++ src/main/java/dk/ek/onidesign/catalog/repository/PackDataRepository.java
@@ -2,7 +2,9 @@ package dk.ek.onidesign.catalog.repository;
 
 import dk.ek.onidesign.catalog.entity.PackData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PackDataRepository extends JpaRepository<PackData, Long> {
     PackData findByModule_ModuleId(Long moduleId);
 }
